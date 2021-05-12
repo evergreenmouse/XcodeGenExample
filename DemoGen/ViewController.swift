bdd31d98c9848d4abed97cf97aa1193f3fd062d1
anopolov <anopolov@mts.ru>
2021-05-12 13:41:26 +0300

    Initial Commit

---
+++ DemoGen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoGen
+//
+//  Created by Ополовников Александр Николаевич on 12.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
